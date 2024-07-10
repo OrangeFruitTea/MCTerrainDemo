24ab45c5ff84368d93ed322be523c985bfedf8fc
OrangeFruitTea <118669479+OrangeFruitTea@users.noreply.github.com>
2024-07-10 21:01:00 +0800

    添加检测六向偏移全局变量

--- Source/MCTerrainDemo/Block.cpp
+++ Source/MCTerrainDemo/Block.cpp
@@ -7,7 +7,7 @@
 ABlock::ABlock()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 
 }
 
@@ -18,10 +18,9 @@ void ABlock::BeginPlay()
 	
 }
 
-// Called every frame
-void ABlock::Tick(float DeltaTime)
+void ABlock::UpdateVisibility()
 {
-	Super::Tick(DeltaTime);
-
+	FVector3d Location = GetActorLocation();
 }
+
 
--- Source/MCTerrainDemo/Block.h
+++ Source/MCTerrainDemo/Block.h
@@ -6,6 +6,19 @@
 #include "GameFramework/Actor.h"
 #include "Block.generated.h"
 
+// block的六向偏移，辅助检测周边是否有其他方块遮挡
+inline double GFaceDir_Offset_Offset = 125.f;
+inline FVector3d GFaceDir_Offset_Fwd = FVector3d(0.f, GFaceDir_Offset_Offset, 0.f);
+inline FVector3d GFaceDir_Offset_Bwd = -GFaceDir_Offset_Fwd;
+inline FVector3d GFaceDir_Offset_R = FVector3d(-GFaceDir_Offset_Offset, 0.f, 0.f);
+inline FVector3d GFaceDir_Offset_L = -GFaceDir_Offset_R;
+inline FVector3d GFaceDir_Offset_Up = FVector3d(0.f ,0.f, GFaceDir_Offset_Offset);
+inline FVector3d GFaceDir_Offset_Dwn = -GFaceDir_Offset_Dwn;
+inline FVector3d GFaceDir_Offset_Dir[6] = {GFaceDir_Offset_Fwd, GFaceDir_Offset_Bwd, GFaceDir_Offset_R, GFaceDir_Offset_L, GFaceDir_Offset_Up,GFaceDir_Offset_Dwn};
+
+inline FString FilePath_M_B_Translucent = TEXT("/Game/MCTerrainDemo/Content/Materials/Blocks/M_B_Translucent");
+inline UMaterialInterface* M_Translucent = LoadObject<UMaterialInterface>(nullptr, *FilePath_M_B_Translucent);
+
 UCLASS()
 class MCTERRAINDEMO_API ABlock : public AActor
 {
@@ -19,8 +32,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+	void UpdateVisibility();
 
 };
